659f8ad8e3d85dbdb054d59409948b546645698e
PercyJW-2 <github@mail.joris-wachsmuth.de>
2025-08-12 22:56:49 +0200

    fixed incorrect name

--- drivers/led_strip/ws2812_gpio.c
+++ drivers/led_strip/ws2812_gpio.c
@@ -232,7 +232,7 @@ static const uint8_t ws2812_gpio_##idx##_color_mapping[] =		\
 	WS2812_COLOR_MAPPING(idx);					\
 									\
 	static const struct ws2812_gpio_cfg ws2812_gpio_##idx##_cfg = { \
-		.gpio = GPIO_DT_SPEC_INST_GET(idx, in_gpios),	\
+		.gpio = GPIO_DT_SPEC_INST_GET(idx, gpios),		\
 		.num_colors = WS2812_NUM_COLORS(idx),			\
 		.color_mapping = ws2812_gpio_##idx##_color_mapping,	\
 	};								\
